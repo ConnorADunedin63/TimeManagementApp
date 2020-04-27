0e49635f57a9d3ad0754ac7a1aaa451317ba3c28
Connor <connora.wellington18@gmail.com>
2020-04-28 09:36:12 +1200

    Started adding checklist to createGoals screen
    Currently there is an array of JSON objects which make up the different tasks in the checklist {name, complete}. The issue is that react native does not detect a state change when the JSON objects inside the array are modified. I am currently working on a way to copy the array so that a state change will be detected.

--- code/TimeManagementApp/pages/createGoals.js
+++ code/TimeManagementApp/pages/createGoals.js
@@ -31,6 +31,7 @@ export default function createGoal({ navigation }) {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [date, setDate] = useState(new Date());
+  const[checklist, setChecklist] = useState([]);
 
   return (
     <View style={styles.container}>
@@ -41,6 +42,7 @@ export default function createGoal({ navigation }) {
         <TextInput style={styles.formInput} placeholder="Name (required)" onChangeText={text => setName(text)}></TextInput>
         <TextInput style={styles.formInput} placeholder="Description (optional)" onChangeText={text => setDescription(text)}></TextInput>
         {datePicker(date, setDate)}
+        {goalChecklist(checklist, setChecklist)}
         <View style={styles.createContainer}>
           <Button title="Create Goal"
           onPress={() => {setGoals(name, description, date); navigation.navigate('Home')}}
@@ -51,6 +53,12 @@ export default function createGoal({ navigation }) {
   );
 }
 
+/**
+  Renders the date picker for selecting a end date for a goal
+  param date: The current Date object that is being used or "N/A" if the goal does not have an end date
+  param setDate: The function used to set the date for the goal
+  return React-Native components: The date picker react components
+*/
 function datePicker(date, setDate) {
     const [mode, setMode] = useState('date');
     const [show, setShow] = useState(false);
@@ -79,7 +87,9 @@ function datePicker(date, setDate) {
 
     return (
     <View>
-      <Text style={{color: 'white'}}>Has End Date?</Text>
+      <View style={{marginTop: 10}}>
+        <Text style={{color: 'white'}}>Has End Date?</Text>
+      </View>
       <View style={{flexDirection: 'row', alignItems: 'stretch'}}>
         <View style={{flex: 1, marginRight: 10}}>
           <Button title='Yes' color={hasEndDate ? 'green' : ''} onPress={() => {setEndDate(true); setDate(new Date())}} />
@@ -110,4 +120,70 @@ function datePicker(date, setDate) {
       )}
     </View>
   );
+}
+
+/**
+  Returns the react components that are used to render the checklist
+  param checklist: An array of JSON objects that represents tasks, with a name and boolean
+  param setChecklist: The function that is used to set the checklist
+*/
+function goalChecklist(checklist, setChecklist) {
+  const [hasChecklist, setHasChecklist] = useState(false);
+  const [task, setTask] = useState('');
+
+  return (
+    <View>
+      <View style={{marginTop: 10}}>
+        <Text style={{color: 'white'}}>Has Checklist?</Text>
+      </View>
+      <View style={{flexDirection: 'row', alignItems: 'stretch'}}>
+        <View style={{flex: 1, marginRight: 10}}>
+          <Button title='Yes' color={hasChecklist ? 'green' : ''} onPress={() => {setHasChecklist(true); setChecklist([])}} />
+        </View>
+        <View style={{flex: 1}}>
+          <Button title='No' color={hasChecklist === false ? 'green' : ''} onPress={() => {setHasChecklist(false); setChecklist(null)}} />
+        </View>
+      </View>
+      <View style={hasChecklist === false ? {display : 'none'} : ''}>
+        <View style={{flexDirection: 'row', alignItems: 'stretch'}}>
+          <View style={{flex: 2, marginRight: 10, marginTop: 10}}>
+            <TextInput placeholder='Task name'
+            style={{backgroundColor: 'white'}}
+            value={task}
+            onChangeText={text => {setTask(text)}}/>
+          </View>
+          <View style={{flex: 1, marginTop: 15}}>
+            <Button title='Create task' onPress={() => {checklist.push({name: task, complete: false}); setTask('')}} />
+          </View>
+        </View>
+        {checklist !== null ? displayTasks(checklist, setChecklist) : null}
+      </View>
+    </View>
+  );
+}
+
+/**
+  Displays the current checklist
+*/
+function displayTasks(checklist, setChecklist) {
+  let checklistItems = [];
+  for(let i = 0; i < checklist.length; i ++) {
+    checklistItems.push(
+      <View style={{flexDirection: 'row', marginTop: 10, marginRight: 10}} key={i}>
+        <View style={{flex: 2, marginTop: 8}}>
+          <Text style={{color: 'white'}}>{checklist[i].name}</Text>
+        </View>
+        <View style={{flex: 2}}>
+          <Button
+          title={checklist[i].complete ? 'Complete' : 'Not Complete'}
+          color={checklist[i].complete ? 'green' : 'red'}
+          onPress={() => {
+            let newChecklist = checklist.splice();
+            console.log(checklist);
+          }} />
+        </View>
+      </View>
+    );
+  }
+  return checklistItems;
 }
