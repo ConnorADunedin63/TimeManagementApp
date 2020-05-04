ce8a86272d3a102a6e862032a2a395e35b5b0c0e
Connor <connora.wellington18@gmail.com>
2020-05-04 18:07:17 +1200

    Added Drawer navigator
    This will be used to switch between the diferent features. This has broken one of the tests which I am working on fixing.

--- code/TimeManagementApp/pages/App.js
+++ code/TimeManagementApp/pages/App.js
@@ -10,6 +10,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {
   Button,
   SafeAreaView,
@@ -34,16 +35,25 @@ import HomeScreen from './home.js';
 import CreateGoal from './createGoals.js';
 import EditGoal from './editGoal.js';
 
-const Stack = createStackNavigator();
-
 export default function App() {
+  const Drawer = createDrawerNavigator();
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen options={{headerShown: false}} name="Home" component={HomeScreen} />
-        <Stack.Screen name="Create Goal" component={CreateGoal} />
-        <Stack.Screen name="Edit Goal" component={EditGoal} />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Goals">
+        <Drawer.Screen options={{headerShown: false}} name="Goals" component={Goals} />
+      </Drawer.Navigator>
     </NavigationContainer>
+  );
+}
+
+function Goals() {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen options={{headerShown: false}} name="Home" component={HomeScreen} />
+      <Stack.Screen name="Create Goal" component={CreateGoal} />
+      <Stack.Screen name="Edit Goal" component={EditGoal} />
+    </Stack.Navigator>
   );
 }
